7dbad1a136e02fe884054432258e2e82d3988807
cys2356 <cys18499@gmail.com>
2023-12-13 09:51:16 +0900

    2023 - 12 - 13 최예성

--- src/main/java/com/clean/cleanssakssak/common/Const.java
+++ src/main/java/com/clean/cleanssakssak/common/Const.java
@@ -5,6 +5,10 @@ public class Const {// 주로 사용되는 응답값 상수 정의
     public static final int TODO_ROW_COUNT = 8;// todo 페이징 처리
     public static final int DIARY_ROW_COUNT = 10;// diary 페이징 처리
 
+    public static final int DIARY_TITLE_MISSING = -1;
+    public static final int DIARY_PICS_MISSING = -2;
+
+
     public static final int FAIL = 0;// 실패 시 응답값
     public static final int SUCCESS = 1;// 성공 시 응답값
 
--- src/main/java/com/clean/cleanssakssak/diary/DiaryService.java
+++ src/main/java/com/clean/cleanssakssak/diary/DiaryService.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+import static com.clean.cleanssakssak.common.Const.*;
+
 @Service
 @RequiredArgsConstructor
 public class DiaryService {
@@ -15,10 +17,16 @@ public class DiaryService {
     // 다이어리 작성 , 제목 사진 내용 생성일자 수정일자
     public ResVo postDiary(DiaryInsDto dto) {
         if (dto.getTitle() == null || dto.getTitle().isBlank()) {
-            return new ResVo(-1); // 제목 null 값이거나 제목 빈 문자열 일 경우 -1
+            return new ResVo(DIARY_TITLE_MISSING); // 제목 null 값이거나 제목 빈 문자열 일 경우 -1
         }
-        int result = mapper.insDiary(dto);
+        if (dto.getPics().stream().anyMatch(pics -> pics == null || pics.isBlank())) { //https://peterica.tistory.com/11
+            // stream : 리스트나 배열을 스트림으로 변환 (사진의 주소값)
+            // anymatch 특정 조건을 만족하는 요소가 하나라도 있으면 true 반환하고 실행 X
+            // pics -> pics 람다 표현식,변수명상관x, 문자열을 pic 으로 받아들임, null값이거나 빈문자열 확인 뭔말임?
+            return new ResVo(DIARY_PICS_MISSING); // pics의 값 중에 null이거나 빈 문자열이 있는 경우 -2
+        }
 
+        int result = mapper.insDiary(dto);
         if (result == 1){ // 제대로 작성이 완료 했으면 사진넣는 작업
             List<String> pics = dto.getPics(); //다이어리 작성에 사용할 사진 가져와서
 
@@ -43,7 +51,7 @@ public class DiaryService {
     // 다이어리 수정
     public ResVo patchDiary(DiaryUpdDto dto) {
         if (dto.getTitle() == null || dto.getTitle().isBlank()) {
-            return new ResVo(-1); // 제목 null 값이거나 제목 빈 문자열 일 경우 -1
+            return new ResVo(DIARY_TITLE_MISSING); // 제목 null 값이거나 제목 빈 문자열 일 경우 -1
         }
         int result = mapper.UpdDiary(dto);
         if (result == 1) { //수정 성공시
@@ -55,14 +63,10 @@ public class DiaryService {
                 picDto.setPics(dto.getPics()); // 사진 설정
                 int picInsResult = mapper.insDiaryPic(picDto); //사진정보 전달
 
-                if (picInsResult == 0) {
-                    return new ResVo(0); // 사진 실패 0
-                }
             }
         }
         return new ResVo(result); // 성공 1 실패 0
     }
-
 
     // 다이어리 페이징 처리
     public List<DiarySelVo> getDiary(DiarySelDto dto) {
