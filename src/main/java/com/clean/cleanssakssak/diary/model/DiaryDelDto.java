075c43e1661e80ada4c61229ac941527420cefe0
cys2356 <cys18499@gmail.com>
2023-12-13 12:02:54 +0900

    2023 - 12 - 13 최예성

--- src/main/java/com/clean/cleanssakssak/diary/model/DiaryDelDto.java
+++ src/main/java/com/clean/cleanssakssak/diary/model/DiaryDelDto.java
@@ -1,5 +1,6 @@
 package com.clean.cleanssakssak.diary.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
@@ -8,6 +9,7 @@ import lombok.Data;
 @Builder
 @Schema(title = "다이어리 삭제 시 필요한 데이터")
 public class DiaryDelDto {
+
     @Schema(title = "로그인 한 유저 pk")
     private int loginedUserId;
     @Schema(title = "다이어리 번호")
