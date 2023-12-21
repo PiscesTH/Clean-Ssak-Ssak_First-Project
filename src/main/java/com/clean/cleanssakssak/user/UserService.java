7a2512fce5236f5f48880ae682f7e628f9061efe
hyun <kimkh8056k@gmail.com>
2023-12-21 16:37:08 +0900

    23/12/21 경현

--- src/main/java/com/clean/cleanssakssak/user/UserService.java
+++ src/main/java/com/clean/cleanssakssak/user/UserService.java
@@ -24,7 +24,7 @@ public class UserService {
     private final DiaryMapper diaryMappermapper;
 
 
-    // 회원가입 메소드
+    //----------------------------------------------회원가입 메소드-----------------------------------------------
     public ResVo postSignup(UserInsSignupDto dto){
 
 /*
@@ -47,8 +47,17 @@ public class UserService {
             return new ResVo(Const.NOT_ALLOWED_NICKNAME);//-4
         }
 
-        int idCheck = userMapper.selIdComparison(dto.getUid());//ID 중복 체크
-        Integer nmCheck = userMapper.selUserByNickname(dto.getNickname());// 닉네임 중복 체크
+        int idCheck;
+        Integer nmCheck;
+
+        try {
+
+            idCheck = userMapper.selIdComparison(dto.getUid());//ID 중복 체크
+            nmCheck = userMapper.selUserByNickname(dto.getNickname());// 닉네임 중복 체크
+
+        }catch (Exception e){
+            return new ResVo(Const.INTERNAL_SERVER_ERROR);
+        }
 
         if(idCheck == 1){//ID 중복으로 회원가입 실패 시 응답값
             return new ResVo(Const.ID_DUPLICATED);
@@ -59,14 +68,19 @@ public class UserService {
         }
 
         dto.setUpw(BCrypt.hashpw(dto.getUpw(),BCrypt.gensalt()));// 비밀번호 암호화
-        int result = userMapper.insUserSignup(dto);
-        // ID와 닉네임이 중복되지 않으니 요청 값으로 INSERT(회원가입) 진행
+
+        try {
+            int result = userMapper.insUserSignup(dto);
+            // ID와 닉네임이 중복되지 않으니 요청 값으로 INSERT(회원가입) 진행
+        }catch (Exception e){
+            return new ResVo(Const.INTERNAL_SERVER_ERROR);
+        }
 
         return new ResVo(dto.getUserId());
         //위의 IF문에 해당되지 않는다면 INSERT 성공으로 해당 user_id값 리턴
     }
 
-    // 로그인 인증 메소드
+    //----------------------------------------로그인 인증 메소드----------------------------------------
     public UserSigninVo postSignin(UserSigninDto dto){
 
         UserSigninVo vo = new UserSigninVo();
@@ -76,17 +90,22 @@ public class UserService {
             return vo;
         }
 
-        String hashedPassword = userMapper.selSigninPw(dto);// 받아온 유저의 uid값을 이용해 해당 upw를 SELECT
+        try {
 
-        if(hashedPassword == null){//SELECT 하지 못한 것 = 해당 uid가 없다
-            vo.setResult(Const.LOGIN_FAILED_BY_UID);
-            return vo;
-        }
-        if(BCrypt.checkpw(dto.getUpw(), hashedPassword)){
-            //upw를 SELECT 했다면 암호화 된 password가 고객이 입력한 dto.getUpw와 같은지 체크
-            vo = userMapper.selSigninInfo(dto);//true라면 로그인 성공 해당 유저의 정보를 SELECT
-            vo.setResult(Const.SUCCESS);
-            return vo;
+            String hashedPassword = userMapper.selSigninPw(dto);// 받아온 유저의 uid값을 이용해 해당 upw를 SELECT
+
+            if(hashedPassword == null){//SELECT 하지 못한 것 = 해당 uid가 없다
+                vo.setResult(Const.LOGIN_FAILED_BY_UID);
+                return vo;
+            }
+            if(BCrypt.checkpw(dto.getUpw(), hashedPassword)){
+                //upw를 SELECT 했다면 암호화 된 password가 고객이 입력한 dto.getUpw와 같은지 체크
+                vo = userMapper.selSigninInfo(dto);//true라면 로그인 성공 해당 유저의 정보를 SELECT
+                vo.setResult(Const.SUCCESS);
+                return vo;
+            }
+        }catch (Exception e){
+            return null;
         }
 
         vo.setResult(Const.LOGIN_FAILED_BY_UPW);//위의 IF문에 다 해당되지 않는다면 upw가 틀렸다는 뜻
@@ -94,42 +113,64 @@ public class UserService {
         return vo;
     }
 
-    //유저 회원정보(비밀번호, 닉네임) 변경 처리
+    //-----------------------------------유저 회원정보(비밀번호, 닉네임) 변경 처리-----------------------------------
     public ResVo patchProfile(UserUbdDto dto){
+
         if (!StringUtils.hasText(dto.getUpw()) && !StringUtils.hasText(dto.getNickname())){//비밀번호, 닉네임 둘 다 값이 없을 경우
             return new ResVo(Const.NOT_ALLOWED_ID + Const.NOT_ALLOWED_PW);//-6
         }
         if (StringUtils.hasText(dto.getUpw()) && StringUtils.containsWhitespace(dto.getUpw())) {//수정할 비밀번호 데이터에 공백이 포함되어 있다
             return new ResVo(Const.NOT_ALLOWED_PW);//-4
         }
+
         int updResult = 0;
+
+        try{
         if (StringUtils.hasText(dto.getUpw())){//수정할 비밀번호 데이터가 제대로 들어온 경우
             String hashedUpw = BCrypt.hashpw(dto.getUpw(),BCrypt.gensalt());//비밀번호 암호화
             dto.setUpw(hashedUpw);
             updResult += userMapper.updUserUpw(dto);//비밀번호 수정
+        }}catch (Exception e){
+            return new ResVo(Const.INTERNAL_SERVER_ERROR);
         }
 
         if(!StringUtils.hasText(dto.getNickname())){
             return new ResVo(updResult);
         }
 
+        try{
+
         Integer nicknameCheck = userMapper.selUserByNickname(dto.getNickname());//닉네임 중복 체크용
 
         if (nicknameCheck == null){
             //닉네임 중복이 없고 수정할 닉네임 데이터가 제대로 들어온 경우
             updResult += userMapper.updUserNickname(dto);//닉네임 수정
+
+        }}catch(Exception e){
+            return new ResVo(Const.INTERNAL_SERVER_ERROR);
         }
 
         return new ResVo(updResult);
         // result = 0 : 수정 실패 / result = 1 : ( 수정 한 개만 시도 시 ) 수정 성공 /result = 2 : ( 수정 두 개 시도 시 )변경 성공
     }
 
-    //유저 회원탈퇴 처리
+    //-------------------------------------------유저 회원탈퇴 처리----------------------------------------
     public ResVo delProfile(int loginedUserId){
-        int delDiaryPicResult = diaryMappermapper.delDiaryPicForUnregister(loginedUserId);//회원탈퇴 할 유저의 다이어리 사진 삭제
-        int delDiaryResult = diaryMappermapper.delDiaryForUnregister(loginedUserId);//회원탈퇴 할 유저의 다이어리 삭제
-        int delTodoResult = todoMappermapper.delTodoForUnregister(loginedUserId);//회원탈퇴 할 유저의 todo 삭제
-        int delResult = userMapper.delUser(loginedUserId);//회원탈퇴 할 유저의 정보 삭제
+
+        int delResult;
+
+        try {
+
+            int delDiaryPicResult = diaryMappermapper.delDiaryPicForUnregister(loginedUserId);//회원탈퇴 할 유저의 다이어리 사진 삭제
+            int delDiaryResult = diaryMappermapper.delDiaryForUnregister(loginedUserId);//회원탈퇴 할 유저의 다이어리 삭제
+            int delTodoResult = todoMappermapper.delTodoForUnregister(loginedUserId);//회원탈퇴 할 유저의 todo 삭제
+            delResult = userMapper.delUser(loginedUserId);//회원탈퇴 할 유저의 정보 삭제
+
+        }catch(Exception e){
+            return new ResVo(Const.INTERNAL_SERVER_ERROR);
+        }
+
         return new ResVo(delResult);
+
     }
 }
