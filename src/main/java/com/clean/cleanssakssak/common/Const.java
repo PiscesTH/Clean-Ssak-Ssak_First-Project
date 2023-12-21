aede0f591c66c64f2a30234fc793198db555bc79
TH <kareme007@gmail.com>
2023-12-21 10:55:07 +0900

    23.12.21. 태형

--- src/main/java/com/clean/cleanssakssak/common/Const.java
+++ src/main/java/com/clean/cleanssakssak/common/Const.java
@@ -11,13 +11,12 @@ public class Const {// 주로 사용되는 응답값 상수 정의
     public static final int SUCCESS = 1;// 성공 시 응답값
 
     public static final int ID_DUPLICATED = -1; // usersignup ID가 중복됨
-    public static final int NICKNAME_DUPLICATED = 0; // usersignup 닉네임이 중복됨
     public static final int NOT_ALLOWED_ID = -2; // usersignup ID 공란 또는 null
     public static final int NOT_ALLOWED_NICKNAME = -3; // usersignup 닉네임 공란 또는 null
     public static final int NOT_ALLOWED_PW = -4; // usersignup PW 공란 또는 null
-    public static final int ID_PW_BLANK = -5; // usersignup ID, PW 중에 또는 둘 다 문자 사이 공란이 있다
     public static final int LOGIN_FAILED_BY_UID = 2; // usersignin ID가 다르다
     public static final int LOGIN_FAILED_BY_UPW = 3; // usersignin PW가 다르다
+    public static final int NICKNAME_DUPLICATED = -5; // usersignup 닉네임이 중복됨
 
     public static final int CHECK_ON = 1; //check시 is_checked에 들어갈 값
     public static final int CHECK_OFF = 0; //check 삭제 시 is_checked에 들어갈 값
